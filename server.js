ae397d41127eb4e08a079298fba7806f129f80eb
afrosefathima455-hub <afrosefathima455@gmail.com>
2026-06-12 09:44:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Task = require("./models/Task");
+require("dotenv").config();
+
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Task Manager API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+
+    if (!deletedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
